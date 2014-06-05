5896f11619d8ad73bc6d0e005d1f90ca14d2666f
zgong5 <zhipeng.gong@intel.com>
2014-06-06 06:09:43 +0800

    drm/i915: Implement WaDisableFenceDestinationToSLM:bdw
    HDC_CHICKEN0 bit 14 (Fence Destination To SLM Disable) must be
    programmed by software to 1h (Disable) to work around a LSLM unit issue.
    WaDisableFenceDestinationToSLM is only needed for BDW E,F step and
    CHV Ax step. Therefore it can be reverted at a later time.
    Issue: APDEV-3096
    Change-Id: Ic7fc5a36d20aca863ab879420a78c5ff67d07860
    Signed-off-by: Michel Thierry <michel.thierry@intel.com>

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -4843,8 +4843,10 @@ static void gen8_init_clock_gating(struct drm_device *dev)
 	 * workaround for for a possible hang in the unlikely event a TLB
 	 * invalidation occurs during a PSD flush.
 	 */
+	/* WaDisableFenceDestinationToSLM:bdw */
 	I915_WRITE(HDC_CHICKEN0,
 		   I915_READ(HDC_CHICKEN0) |
+		   _MASKED_BIT_ENABLE(HDC_FENCE_DESTINATION_TO_SLM_DISABLE) |
 		   _MASKED_BIT_ENABLE(HDC_FORCE_NON_COHERENT));
 
 	/* WaVSRefCountFullforceMissDisable:bdw */
