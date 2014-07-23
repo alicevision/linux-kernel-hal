328d964fe373b707437595874a51d39891b7299b
Zhipeng Gong <zhipeng.gong@intel.com>
2014-07-23 09:01:40 +0800

    Revert "drm/i915: Prevent recursive deadlock on releasing a busy userptr"
    This reverts commit 22deb610163c390553ecda22fe72ca0058469a1e.

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -164,7 +164,6 @@ enum hpd_pin {
 		if ((intel_encoder)->base.crtc == (__crtc))
 
 struct drm_i915_private;
-struct i915_mm_struct;
 struct i915_mmu_object;
 
 enum intel_dpll_id {
@@ -1484,8 +1483,9 @@ typedef struct drm_i915_private {
 	struct i915_gtt gtt; /* VMA representing the global address space */
 
 	struct i915_gem_mm mm;
-	DECLARE_HASHTABLE(mm_structs, 7);
-	struct mutex mm_lock;
+#if defined(CONFIG_MMU_NOTIFIER)
+	DECLARE_HASHTABLE(mmu_notifiers, 7);
+#endif
 
 	/* Kernel Modesetting */
 
@@ -1740,8 +1740,8 @@ struct drm_i915_gem_object {
 			unsigned workers :4;
 #define I915_GEM_USERPTR_MAX_WORKERS 15
 
-			struct i915_mm_struct *mm;
-			struct i915_mmu_object *mmu_object;
+			struct mm_struct *mm;
+			struct i915_mmu_object *mn;
 			struct work_struct *work;
 		} userptr;
 	};
