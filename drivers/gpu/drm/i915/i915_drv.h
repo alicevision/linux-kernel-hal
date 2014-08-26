acccfe89434b7ed2ccec9bd9e8a6f2af094fbba8
Arun Siluvery <arun.siluvery@linux.intel.com>
2014-08-26 14:44:51 +0100

    drm/i915/bdw: Export workaround data to debugfs
    The workarounds that are applied are exported to a debugfs file;
    this is used to verify their state after the test case (reset or
    suspend/resume etc). This patch is only required to support i-g-t.
    
    Signed-off-by: Arun Siluvery <arun.siluvery@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    
    Conflicts:
    	drivers/gpu/drm/i915/i915_debugfs.c

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -1504,6 +1504,20 @@ typedef struct drm_i915_private {
 	struct intel_ddi_plls ddi_plls;
 	int dpio_phy_iosf_port[I915_NUM_PHYS_VLV];
 
+	/*
+	 * workarounds are currently applied at different places and
+	 * changes are being done to consolidate them so exact count is
+	 * not clear at this point, use a max value for now.
+	 */
+#define I915_MAX_WA_REGS  16
+	struct {
+		u32 addr;
+		u32 value;
+		/* bitmask representing WA bits */
+		u32 mask;
+	} intel_wa_regs[I915_MAX_WA_REGS];
+	u32 num_wa_regs;
+
 	/* Reclocking support */
 	bool render_reclock_avail;
 	bool lvds_downclock_avail;
